cef22dcf5bde350962630c79072db5451800471e
saray <sm78544@lasallebajio.edu.mx>
2025-03-12 16:55:48 -0600

    added comits to the HomeScreen

--- app/src/main/java/com/example/smartinezfoodapp/screen/HomeScreen.kt
+++ app/src/main/java/com/example/smartinezfoodapp/screen/HomeScreen.kt
@@ -1,23 +1,10 @@
 package com.example.smartinezfoodapp.screen
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
@@ -26,18 +13,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 import com.example.smartinezfoodapp.componants.BestSellers
 import com.example.smartinezfoodapp.componants.CategoryItem
 import com.example.smartinezfoodapp.componants.Greetings
 import com.example.smartinezfoodapp.componants.RestItems
-import com.example.smartinezfoodapp.icons.Logout
-import com.example.smartinezfoodapp.icons.StarFilled
-import com.example.smartinezfoodapp.icons.UserRound
-import com.example.smartinezfoodapp.models.categoryList
-import com.example.smartinezfoodapp.models.foodList
-import com.example.smartinezfoodapp.models.restaurantList
-import com.example.smartinezfoodapp.ui.theme.Orange
 import com.example.smartinezfoodapp.ui.theme.SMartinezFoodAppTheme
 
 @OptIn(ExperimentalGlideComposeApi::class)
@@ -63,7 +42,7 @@ fun HomeScreen(innerPadding: PaddingValues) {
             modifier = Modifier
                 .padding(2.dp)
         )
-
+        // Gathers the layout from file Category
         CategoryItem()
 
         Spacer(modifier = Modifier.height(2.dp))
@@ -78,7 +57,7 @@ fun HomeScreen(innerPadding: PaddingValues) {
             modifier = Modifier
                 .padding(8.dp)
         )
-
+        // Gathers the layout from file RestItems
         RestItems()
 
         Spacer(modifier = Modifier.height(2.dp))
@@ -91,7 +70,7 @@ fun HomeScreen(innerPadding: PaddingValues) {
             modifier = Modifier
                 .padding(2.dp)
         )
-
+        // gathers the layout from file BestSeller
         BestSellers()
 
 
